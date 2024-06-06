9cfd5a1d04b75714b3b949622368e8c6d3627def
0poss <67114640+0poss@users.noreply.github.com>
2024-06-07 00:57:10 +0200

    Add bindings/

---
+++ project-rs/bindings/project-rs.hpp
@@ -0,0 +1,18 @@
+#ifndef PROJECT_RS_H
+#define PROJECT_RS_H
+
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+extern "C" {
+
+extern void hello_from_cpp();
+
+void hello_from_rust();
+
+} // extern "C"
+
+#endif // PROJECT_RS_H
